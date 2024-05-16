51fb71408d8be9abec09e48f0a7cd0bbfec74152
brylan-the-shark <162459810+brylan-the-shark@users.noreply.github.com>
2024-05-16 21:14:34 +0200

    Commit 1 Add adding students functionality

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from service import Service
 from student import Student
+from menu import menu, pause
+
+def input_student():
+	try:
+		s.add_student(Student(input('Name: '), int(input('Age: '))))
+		print('Added student to the database. Returning to main menu.')
+	except Exception as e:
+		print(f'An error occurred: {e}')
+	pause()
 
 if __name__ == '__main__':
-    try:
-        s = Service()
-        s.add_student(Student('Krzysztof', 20))
-        s.add_student(Student('Janusz', 40))
-
-        students = s.get_students()
-        for current in students:
-            print(current)
-    except IOError as e:
-        print(f'An error occurred: {e}')
+	try:
+		s = Service()
+		
+		while True:
+			match menu('Add studend'):
+				case 0:
+					input_student()
+	except IOError as e:
+		print(f'An error occurred: {e}')
