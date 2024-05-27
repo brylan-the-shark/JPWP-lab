3a751c8adc39912b4ae86ea004755b004a3b514d
brylan-the-shark <162459810+brylan-the-shark@users.noreply.github.com>
2024-05-27 19:52:15 +0200

    Use tabulator as the separator. Fix spaces screwing with parsing

--- student.py
+++ student.py
@@ -7,11 +7,11 @@ class Student:
 	birthday : str
 
 	def __str__(self):
-		return f'{self.name} {self.age} {self.birthday}'
+		return f'{self.name}\t{self.age}\t{self.birthday}'
 
 	@staticmethod
 	def parse(str):
-		data = str.split(' ')
+		data = str.split('\t')
 		if len(data) != 3:
 			return Student('Old/invalid format', -1, 'None')
 		return Student(data[0], int(data[1]), data[2])
