2d46277be3b0d101898bc0df88938f187b715b2a
brylan-the-shark <162459810+brylan-the-shark@users.noreply.github.com>
2024-05-27 22:09:23 +0200

    Commit 8 Value errors for invalid input in `Student`

--- student.py
+++ student.py
@@ -1,10 +1,21 @@
-from dataclasses import dataclass
+import re
 
-@dataclass
 class Student:
 	name : str
 	age : int
 	birthday : str
+
+	def __init__(self, name, age, birthday):
+		if not re.fullmatch('[A-Z][a-z]*', name):
+			raise ValueError('Name must only contain letters and be capitalized')
+		if age < 0 or age > 150:
+			raise ValueError('Age must be in range 0 to 150')
+		if not re.fullmatch('\d{2}-\d{2}-\d{4}', birthday):
+			raise ValueError('Birthday must be in DD-MM-YYYY format')
+
+		self.name = name
+		self.age = age
+		self.birthday = birthday
 
 	def __str__(self):
 		return f'{self.name}\t{self.age}\t{self.birthday}'
@@ -12,10 +23,15 @@ class Student:
 	@staticmethod
 	def parse(str):
 		data = str.split('\t')
+		
 		if len(data) != 3:
 			return Student.invalid()
-		return Student(data[0], int(data[1]), data[2])
+		
+		try: ret = Student(data[0], int(data[1]), data[2])
+		except: return Student.invalid()
+		
+		return ret
 	
 	@staticmethod
 	def invalid():
-		return Student('Old/invalid format', -1, 'None')
+		return Student('Invalid', 0, '00-00-0000')
